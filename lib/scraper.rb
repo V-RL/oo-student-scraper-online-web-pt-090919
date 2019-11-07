d25bb1b4d4ee41d82a310046440661e562984cfd
Brandon Furr <brandon.furr123@gmail.com>
2019-11-07 22:20:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,9 +2,17 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+  
+  @@students = []
+  
 
   def self.scrape_index_page(index_url)
-    
+    Nokogiri::HTML(open(index_url)).css("a").each do |post|
+      student = Student.new 
+      student.name = post.css("h4").text
+      student.location = post.css("p").text
+      student.profile_url = post.css("a").text
+    end
   end
 
   def self.scrape_profile_page(profile_url)
